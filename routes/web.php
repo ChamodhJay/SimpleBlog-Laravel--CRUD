9cc51456ac5156b69b68691ca608344d7a34fac3
Nimesha Chamodh <143405992+ChamodhJay@users.noreply.github.com>
2023-10-24 18:35:20 +0530

    Route Groups

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\postController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,13 +27,21 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 
 //posts
-Route::post('posts/store',[postController::class,'store'])->name('posts.store');
 Route::get('posts/{postId}/show',[postController::class,'show'])->name('posts.show');
-Route::get('posts/all',[HomeController::class,'allpost'])->name('posts.all');
-Route::get('posts/{postId}/edit',[postController::class,'edit'])->name('posts.edit');
-Route::post('posts/{postId}/update',[postController::class,'update'])->name('posts.update');
-Route::get('posts/{postId}/delete',[postController::class,'delete'])->name('posts.delete');
+Route::group(['middleware'=>'auth'],function(){
+
+    Route::post('posts/store',[postController::class,'store'])->name('posts.store');
+
+    Route::get('posts/all',[HomeController::class,'allpost'])->name('posts.all');
+    Route::get('posts/{postId}/edit',[postController::class,'edit'])->name('posts.edit');
+    Route::post('posts/{postId}/update',[postController::class,'update'])->name('posts.update');
+    Route::get('posts/{postId}/delete',[postController::class,'delete'])->name('posts.delete');
+
+});
 
 //Admin route
-
+Route::group(['middleware'=>['admin','auth']],function(){
+    
 Route::get('/admin/dashboard',[DashboardController::class,'index'])->Middleware('admin')->name('admin.dashboard');
+
+});
